52304d212ada7eb9b47a0f8ff951b66a1f2e2bf2
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2021-03-13 00:59:16 +0300

    ADD description to items in home.js

--- src/components/home/item.js
+++ src/components/home/item.js
@@ -1,12 +1,25 @@
-import React from 'react'
-import itemCss from './item.module.css'
+import React from "react";
+import itemCss from "./item.module.css";
 
-const Item = ({description, image, title}) => {
+const Item = ({ description, image, title }) => {
   return (
-    <section className={`d-flex col-12 col-sm-6 col-lg-4 p-0 m-0 ${itemCss.wrapper} border_custom`}>
-      <div className={`w-100 h-100 border_custom ${itemCss.background}`} style={{background: `url(${image}) no-repeat center/cover`}}></div>
-      <img src={image} alt={`${title} image`} className={`${itemCss.image} mx-auto`} />
+    <section
+      className={`d-flex col-12 col-sm-6 col-lg-4 p-0 m-0 ${itemCss.wrapper} `}
+    >
+      <div
+        className={`w-100 h-100 ${itemCss.background}`}
+        style={{ background: `url(${image}) no-repeat center/cover` }}
+      ></div>
+      <img
+        src={image}
+        alt={`${title} image`}
+        className={`${itemCss.image} mx-auto`}
+      />
+      <div className={`d-flex flex-column w-100 py-1 ${itemCss.description} `}>
+        <h4>{title}</h4>
+        <p>{description}</p>
+      </div>
     </section>
-  )
-}
-export default Item
+  );
+};
+export default Item;
