e449d26e83106f16d4556a3d39a56da425a08e25
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2021-03-13 16:14:56 +0300

    UPDATE read sidebar style

--- src/components/read/sidebar.js
+++ src/components/read/sidebar.js
@@ -4,12 +4,27 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import sidebarCss from './sidebar.module.css'
 
 const Sidebar = () => {
+  const [slide, setSlide] = React.useState('slideRight')
+  const [contentState, setContentState] = React.useState('d-flex')
+  const [arrowState, setArrowState] = React.useState(faArrowLeft)
+
+  const handleToggle = () => {
+    if (slide === 'slideRight') {
+      setSlide('slideLeft')
+      setContentState('d-none')
+      setArrowState(faArrowRight)
+    } else {
+      setSlide('slideRight') 
+      setContentState('d-flex')
+      setArrowState(faArrowLeft)
+    }
+  }
   return (
-    <aside className={`d-flex justify-content-between ${sidebarCss.aside} border_custom`}>
-      <div>Books</div>
-      <div className='border-left'>Chapter</div>
-      <button className={` align-self-center ${sidebarCss.toggle}`}>
-        <FontAwesomeIcon icon={faArrowRight}style={{ color: "White", fontSize: "15px" }}/>
+    <aside className={`row d-flex ${sidebarCss.aside} ${sidebarCss[slide]}`}>
+      <div className={`flex-column text-white col-8 ${contentState}`}><small className='mx-auto'>Books</small></div>
+      <div className={`flex-column text-white text-center border-left border-light px-1 ${contentState}`}><small className='mx-auto'>Chapter</small></div>
+      <button className={` align-self-center ${sidebarCss.toggle}`} onClick={handleToggle}>
+        <FontAwesomeIcon icon={arrowState}style={{ color: "White", fontSize: "15px" }}/>
       </button>
     </aside>
   )
