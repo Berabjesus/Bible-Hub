4fb3df527b180c0cac04ea4cc7eb56baa62f6fb3
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2021-03-13 10:10:16 +0300

    ADD image container for the bible.js individual item

--- src/pages/bible.js
+++ src/pages/bible.js
@@ -1,14 +1,18 @@
 import React from 'react'
+import bibleCss from './bible.module.css'
 
 export const User = props => {
   const {description, image, title} = props.location && props.location.state
-  console.log(description);
   
   return (
-    <div>
-      <h1>User</h1>
-      <h2>{description}</h2>
-    </div>
+    <section className={`position-relative ${bibleCss.wrapper} `}>
+      <div className={`position-absolute w-100 h-100 ${bibleCss.background} `} style={{ background: `url(${image}) no-repeat center/cover` }}>
+
+      </div>
+      <div className={`w-100 h-100` }>
+        <img src={image} alt={`${title} image`} className={`w-100 h-100 ${bibleCss.img}`}/>
+      </div>
+    </section>
   )
 }
 
