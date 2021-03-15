b25d2b36374b599131f24c8ca1e1cf82ed692e3e
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2021-03-15 23:55:19 +0300

    ADD panel to content

---
+++ src/components/read/panel.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import panelCss from './panel.module.css'
+
+const Panel = () => {
+  return (
+    <div className={`${panelCss.bottom}`}>
+      <button>test</button>
+    </div>
+  )
+}
+
+export default Panel
--- src/pages/read.js
+++ src/pages/read.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Sidebar from '../components/read/sidebar';
 import Content from '../components/read/content';
+import Panel from '../components/read/panel'
 
 const Read = props => {
   const { id, title, languages } = props.location && props.location.state;
@@ -10,6 +11,7 @@ const Read = props => {
     <section>
       <Sidebar id={id} setInfo = {setVerseInfo}/>
       <Content info = {verseInfo} />
+      <Panel />
     </section>
   );
 };
