545c9b49013eec5c902d6f43b0be63dbfa7fb848
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2021-03-16 01:28:57 +0300

    ADD footer for content

--- src/components/read/content.js
+++ src/components/read/content.js
@@ -6,21 +6,15 @@ import contentCss from './content.module.css';
 const Content = ({info}) => {
   const contentBgColor = useSelector(state => state.darkMode);
   const data = useSelector(state => state.content);
+  // const [select, setSelect] = React.useState('')
+  const popUp = <div className={`${contentCss.popup}`}>Test</div>
+
   return (
     <article className={`pl-3 pr-1 ${contentCss.article}`} style={contentBgColor}>
       {
         data.content.text && (
         <div>
-          {parse(data.content.text, {
-            replace: function(domNode) {
-              if (domNode.type === 'tag' && domNode.name === 'p') {
-                domNode.attribs = {...domNode.attribs, onClick: ()=>{
-                  let verse = {...{...domNode.children[1].children[0].children}['0']}.data
-                  console.log(info, verse);
-                }}
-              }
-            }
-            })}
+          {parse(data.content.text)}
         </div>
         )
       }
--- src/components/read/panel.js
+++ src/components/read/panel.js
@@ -1,12 +1,26 @@
-import React from 'react'
-import panelCss from './panel.module.css'
-
+import React from "react";
+import panelCss from "./panel.module.css";
+import { faStar as faStarUnchecked } from "@fortawesome/free-regular-svg-icons";
+import { faStar as faStarChecked } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Panel = () => {
+  const [favorite, setFavorite] = React.useState(faStarUnchecked);
+  const handleFavoriteCheck = () => {
+    favorite === faStarUnchecked
+      ? setFavorite(faStarChecked)
+      : setFavorite(faStarUnchecked);
+  };
   return (
-    <div className={`${panelCss.bottom}`}>
-      <button>test</button>
+    <div className={`d-flex align-items-center ${panelCss.bottom}`}>
+      <button className={`mx-auto ${panelCss.button}`}  onClick={handleFavoriteCheck}>
+        <FontAwesomeIcon
+          icon={favorite}
+          style={{ color: "White", fontSize: "25px" }}
+        />
+        <small className="text-white d-block">Favorite</small>
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Panel
+export default Panel;
