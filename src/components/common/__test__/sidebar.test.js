0a1e3eb9935f82f3c724989aaacc68410cfbc23c
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2021-03-17 23:28:27 +0300

    ADD test for sidebar.js

---
+++ src/components/common/__test__/sidebar.test.js
@@ -0,0 +1,75 @@
+import React from "react";
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import renderer from "react-test-renderer";
+import SideBar from "../sideBar";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from 'react-redux';
+import store from '../../../store'
+
+describe("Side bar common component", () => {
+  it("matches the snapshot", () => {
+    const component = renderer.create(
+      <BrowserRouter>
+        <Provider store={store}>
+          <SideBar slide ={'test-class'}/>
+        </Provider>
+      </BrowserRouter>
+    );
+    expect(component).toMatchSnapshot();
+  });
+  it("Renders the side bar correctly", () => {
+    render(
+      <BrowserRouter>
+        <Provider store={store}>
+          <SideBar slide ={'test-class'}/>
+        </Provider>
+      </BrowserRouter>
+    );
+
+    expect(
+      screen.getByText((content, element) => {
+        return (
+          element.tagName.toLowerCase() === "p" &&
+          content.includes('Settings')
+        );
+      })
+    );
+
+    expect(
+      screen.getByText((content, element) => {
+        return (
+          element.tagName.toLowerCase() === "span" &&
+          content.includes('Dark Mode')
+        );
+      })
+    );
+
+    expect(
+      screen.getByText((content, element) => {
+        return (
+          element.tagName.toLowerCase() === "a" &&
+          content.includes('Home')
+        );
+      })
+    );
+
+    expect(
+      screen.getByText((content, element) => {
+        return (
+          element.tagName.toLowerCase() === "a" &&
+          content.includes('Favorites')
+        );
+      })
+    );
+
+    expect(
+      screen.getByText((content, element) => {
+        return (
+          element.tagName.toLowerCase() === "a" &&
+          content.includes('About')
+        );
+      })
+    );
+  });
+});
