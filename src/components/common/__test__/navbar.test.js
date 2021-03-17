f49e04b73a9a41a2691c83f2a086959e57527e0b
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2021-03-17 23:12:19 +0300

    ADD tests for navbar.js

---
+++ src/components/common/__test__/navbar.test.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import renderer from "react-test-renderer";
+import Navbar from "../navbar";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from 'react-redux';
+import store from '../../../store'
+
+describe("Navbar common component", () => {
+  it("matches the snapshot", () => {
+    const component = renderer.create(
+      <BrowserRouter>
+        <Provider store={store}>
+          <Navbar />
+        </Provider>
+      </BrowserRouter>
+    );
+    expect(component).toMatchSnapshot();
+  });
+  it("Renders the Header correctly", () => {
+    render(
+      <BrowserRouter>
+        <Provider store={store}>
+          <Navbar />
+        </Provider>
+      </BrowserRouter>
+    );
+
+    expect(
+      screen.getByText((content, element) => {
+        return (
+          element.tagName.toLowerCase() === "p" &&
+          content.includes("Bible Hub")
+        );
+      })
+    );
+  });
+});
